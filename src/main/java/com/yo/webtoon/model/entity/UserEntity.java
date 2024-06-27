7dd8d30d8d63346c5cd2e50fff0165b92327ccfa
yeon <p-yo00@naver.com>
2024-06-27 23:31:33 +0900

    feat: User 엔티티 기본값 설정
    - 데이터 생성날짜 Auditing
    - 가입 시 포인트 0으로 설정

--- src/main/java/com/yo/webtoon/model/entity/UserEntity.java
+++ src/main/java/com/yo/webtoon/model/entity/UserEntity.java
@@ -1,24 +1,40 @@
 package com.yo.webtoon.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
 public class UserEntity implements UserDetails {
 
     @Id
     private String userId;
     private String password;
     private String userName;
-    private int point;
+    @Builder.Default
+    private int point = 0;
+    @CreatedDate
     private LocalDateTime registerDate;
     private LocalDateTime deleteDate;
     private LocalDateTime adultCertificationDate;
