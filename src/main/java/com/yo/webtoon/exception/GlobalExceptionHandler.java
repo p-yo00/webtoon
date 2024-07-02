1e01b57206507c5b4e4b67ef85d0b5452fa0f316
yeon <p-yo00@naver.com>
2024-07-03 04:53:47 +0900

    feat: 유효성 검증 exception handler 추가

--- src/main/java/com/yo/webtoon/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yo/webtoon/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.yo.webtoon.exception;
 
+import com.yo.webtoon.model.constant.ErrorCode;
 import com.yo.webtoon.model.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +20,21 @@ public class GlobalExceptionHandler {
                 .httpStatus(e.getHttpStatus())
                 .errorCode(e.getErrorCode())
                 .message(e.getErrorCode().getMessage())
+                .build());
+    }
+
+    // @Valid 검증에서 실패했을 때 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleNotValidException(
+        MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .errorCode(ErrorCode.NOT_VALID_INPUT)
+                .message(bindingResult.getFieldErrors().get(0).getDefaultMessage())
                 .build());
     }
 }
--- src/main/java/com/yo/webtoon/model/constant/ErrorCode.java
+++ src/main/java/com/yo/webtoon/model/constant/ErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ErrorCode {
     USER_NOT_FOUND("일치하는 사용자를 찾을 수 없습니다."),
     ALREADY_EXIST_USER_ID("이미 존재하는 사용자 ID 입니다."),
-    FAILED_LOGIN("로그인에 실패했습니다.");
+    FAILED_LOGIN("로그인에 실패했습니다."),
+    NOT_VALID_INPUT("입력값 검증에 실패했습니다.");
 
     private final String message;
 }
